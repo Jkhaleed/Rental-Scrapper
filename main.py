130b027bcd4dd1ba08db7a61ec6e228dc74a7039
Jkhaleed <khaleedjimoh05@gmail.com>
2026-06-06 00:47:02 -0400

    needed to add some other stuff the workflow wasn't working

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import pandas as pd
 
-from Scraper.the_cannon import scrape_the_cannon
-# from Scraper.rentals_ca import scrape_rentals_ca
-from Scraper.places4students import scrape_places4students
-# from Scraper.solstice import scrape_solstice
+from scrapers.the_cannon import scrape_the_cannon
+from scrapers.places4students import scrape_places4students
 
-def main():
-    all_listings = []
+all_listings = []
 
-    all_listings.extend(scrape_the_cannon())
-    # all_listings.extend(scrape_rentals_ca())
-    all_listings.extend(scrape_places4students())
-    # all_listings.extend(scrape_solstice())
+all_listings.extend(scrape_the_cannon())
+all_listings.extend(scrape_places4students())
 
-    df = pd.DataFrame(all_listings)
-    df.drop_duplicates(subset=["Link"], inplace=True)
+df = pd.DataFrame(all_listings)
 
-    df.to_csv("houses.csv", index=False)
+df.drop_duplicates(subset=["Link"], inplace=True)
 
-    print(f"Saved {len(df)} listings")
+df.to_csv("houses.csv", index=False)
 
-if __name__ == "__main__":
-    main()
+print(f"Saved {len(df)} listings")
